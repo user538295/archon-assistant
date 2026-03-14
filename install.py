a3d337f422bfc7ecfaf89aafbb7a40c6a8306583
Gergely Mancz <gergelymancz@gmail.com>
2026-03-15 00:34:46 +0100

    chore: release v26.3.347

--- install.py
+++ install.py
@@ -35,7 +35,7 @@ except ImportError:
     _tomli_w = None  # type: ignore[assignment]
     _HAS_TOMLI_W = False
 
-__version__ = "26.3.345"
+__version__ = "26.3.347"
 
 REPO_URL = "https://github.com/user538295/archon-assistant.git"
 
