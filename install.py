56ce0bf2df73458065be38356543062819d0248d
Gergely Mancz <gergelymancz@gmail.com>
2026-04-09 09:50:34 +0200

    chore: release v26.4.919

--- install.py
+++ install.py
@@ -31,7 +31,7 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
-__version__ = "26.4.907"
+__version__ = "26.4.919"
 
 REPO_URL = "https://github.com/user538295/archon-assistant.git"
 
